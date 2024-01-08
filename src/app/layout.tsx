407058fba3979793d0fb3806b6fe2c6a79e75a05
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-01-08 07:54:11 -0300

    Início de perfil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='bg-red-500 w-full h-screen'>{children}</body>
+      <body className='bg-default w-full h-screen'>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home() {
+import Image from "next/image";
+import ImageBG from "../assets/Image-Background.png"
+
+export default function Profile() {
   return (
-      <h1>Hello world!</h1>
+    <div className="flex flex-col justify-center items-center">
+      <Image src={ImageBG} alt="" style={{ width: '100%', height: '296px' }} />
+      <div className="w-[54rem] h-[220px] bg-default border border-white rounded-md mt-[-60px]">
+      </div>
+    </div>
   )
 }
