45bcf4d00989309007fe4f48688985461da48d41
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-01-09 08:02:50 -0300

    Arquivo octokit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import ImageBG from "../assets/Image-Background.png"
 
 export default function Profile() {
+
+  async function getDataUser() {
+    // const data = await
+  }
+
   return (
     <div className="flex flex-col justify-center items-center">
       <Image src={ImageBG} alt="" style={{ width: '100%', height: '296px' }} />
---
+++ src/utils/octokit.ts
@@ -0,0 +1,5 @@
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+    auth: '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'
+})
